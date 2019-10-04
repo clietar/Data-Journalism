2290d45c0d906891e5e3e380b5621a1ecaeafc3a
Charles Lietar <55199729+spid3rweb@users.noreply.github.com>
2019-10-04 14:41:17 +0200

    Add files via upload

---
+++ #YellowVest NLP/Data Collection/clean_tweets.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+#read the csv we create as a Pandas data frame, and give a column name
+data=pd.read_csv("toulouse.csv")
+data.columns=['tweet']
+
+#replace unwanted characters in each tweet, such as web adress of the tweet, mentioned users, tabulations and #
+data['tweet'] = data['tweet'].str.replace(r'@[A-Za-z0-9-_]+','').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'https?:\/\/.*[\r\n]*', '').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'\n', ' ').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'#', '').astype('object')
+
+#create a new clean csv file with 2 columns : Pandas index and tweets
+data.to_csv("toulouse_clean.csv")
+
+
+df=pd.read_csv("toulouse_clean.csv")
+
+
+
+
