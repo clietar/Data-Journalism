f72a237fcef5fab05bb0d5bf42a141448218db6f
Charles Lietar <55199729+spid3rweb@users.noreply.github.com>
2019-12-07 21:12:41 +0100

    Update clean_tweets.py

--- #YellowVest NLP/Data Collection/clean_tweets.py
+++ #YellowVest NLP/Data Collection/clean_tweets.py
@@ -6,15 +6,18 @@ data=pd.read_csv("your_csv_file.csv")
 data.columns=['tweet']
 
 #replace unwanted characters in each tweet, such as web adress of the tweet, mentioned users, tabulations and #
-data['tweet'] = data['tweet'].str.replace(r'@[A-Za-z0-9-_]+','').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'@[A-Za-z0-9-_]+',' ').astype('object')
 data['tweet'] = data['tweet'].str.replace(r'https?:\/\/.*[\r\n]*', '').astype('object')
 data['tweet'] = data['tweet'].str.replace(r'\n', ' ').astype('object')
-data['tweet'] = data['tweet'].str.replace(r'#', '').astype('object')
-data['tweet'] = data['tweet'].str.replace(r'  ', '').astype('object') # remove blank fields from erased mentioned users
+data['tweet'] = data['tweet'].str.replace(r'#[A-Za-z0-9]+', ' ').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'  ', ' ').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'&lt;', '').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'\"', '').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'[\", ]', ' ').astype('object')
+data['tweet'] = data['tweet'].str.replace(r'\t', '').astype('object')
 
-#create a new clean csv file with 2 columns : Pandas index and tweets
-data.to_csv("your_csv_file_cleaned.csv")
-
-
-
+#save cleaned tweets in a csv (we will use it for Sentiment analysis)
+data.to_csv("tweets_cleaned.csv", index=None)
+#save cleaned tweets in a txt file (we will use it for NLP)
+data.to_csv("tweets_cleaned.txt", header=None, index=None)
 
