fdebb34d6c68455f16be25e5476f5fa6c806f640
Charles Lietar <55199729+spid3rweb@users.noreply.github.com>
2019-12-07 21:32:56 +0100

    Update combine_csv.py

--- #YellowVest NLP/Data Collection/combine_csv.py
+++ #YellowVest NLP/Data Collection/combine_csv.py
@@ -6,5 +6,3 @@ df3 = pd.read_csv('Tweets3.csv', names=['tweet'])
 
 df = pd.concat([df1,df2,df3], axis=0, join='outer', ignore_index=False, keys=None,levels=None, names=None, verify_integrity=False, copy=True)
 df.to_csv('Tweets.csv', index=None, header=None)
-
-df
